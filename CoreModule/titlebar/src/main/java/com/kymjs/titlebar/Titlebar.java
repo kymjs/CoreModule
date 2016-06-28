87eccd467b338de485c7c275e981efab3e35f7b4
张涛 <zhangtao@hujiang.com>
2016-06-28 21:30:27 +0800

    first commit

---
+++ CoreModule/titlebar/src/main/java/com/kymjs/titlebar/Titlebar.java
@@ -0,0 +1,30 @@
+package com.kymjs.titlebar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by ZhangTao on 6/28/16.
+ */
+public class Titlebar extends RelativeLayout {
+
+    public Titlebar(Context context) {
+        super(context);
+        init();
+    }
+
+    public Titlebar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public Titlebar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
